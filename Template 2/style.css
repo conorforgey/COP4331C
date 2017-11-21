5261293d93d0e36e18a28ae06a3cc1351418691b
Zainaaljallad <zaina_aljallad@yahoo.com>
2017-11-21 13:57:17 -0500

    Add files via upload

---
+++ Template 2/style.css
@@ -0,0 +1,89 @@
+.title{
+    margin: 0; 
+    font-family:sans-serif;
+    font-size: 45; 
+    font-weight: bold; 
+}
+div{
+    margin: 50; 
+}
+.body{
+
+    margin-left: 1; 
+}
+
+
+.contact{
+    margin: 0; 
+    font-family: sans-serif;
+    font-size: 15; 
+    font-weight: lighter; 
+    color: green; 
+}
+.object{
+  
+    margin-bottom: 0; 
+    font-family: sans-serif; 
+    font-size: 27; 
+    font-weight: lighter; 
+}
+
+.temp{
+    margin: 0.5; 
+    font-family: sans-serif;
+    font-weight: lighter;
+    color:gray; 
+    font-size: 18;
+    text-align: justify; 
+}
+
+
+
+.experience{ 
+    font-family: sans-serif;
+    font-size: 27;
+    font-weight: lighter;
+    margin-bottom: 0; 
+    
+}
+.job{
+    margin-top: 0.5;
+    font-family: sans-serif;
+    font-weight: lighter;
+    color:gray; 
+    font-size: 18;
+    text-align: justify; 
+}
+.description{
+    margin-top: 0.5; 
+    font-size: 15; 
+    font-family: sans-serif; 
+    color: gray; 
+    font-weight: lighter; 
+ 
+}
+#next{
+    margin-bottom: 0; 
+}
+.school{
+    margin-bottom: 0; 
+    font-family: sans-serif;
+    font-weight: lighter;
+    
+    font-size: 27;
+}
+.award{
+  font-family: sans-serif;
+    font-size: 27;
+    font-weight: lighter;
+    margin : 0; 
+}
+
+
+.more{
+ margin-top: 0.5; 
+    font-size: 15; 
+    font-family: sans-serif; 
+    font-weight: lighter; 
+    color: gray; 
+}
